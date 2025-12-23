8e99315f41e1ae9a8249579a1dadfb96b5ff9fa9
litlesnow <xuehualjx@gmail.com>
2025-12-24 01:25:32 +0800

    feat: Optimize packaging configuration

--- packages/parser/tsup.config.ts
+++ packages/parser/tsup.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  dts: {
-    tsconfig: './tsconfig.build.json',
-    compilerOptions: {
-      composite: false,
-      incremental: false,
-    },
-  },
+  dts: true,
   clean: true,
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   outDir: 'dist',
   target: 'es2020',
   minify: true, // Enable minification
   sourcemap: 'inline',
   shims: true, // Enable shims for import.meta in CJS
+  tsconfig: './tsconfig.build.json',
+  splitting: false,
 });
